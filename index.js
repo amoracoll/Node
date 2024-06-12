9c0e36dbe7e15a6fbfe78a98187048c62fae6c13
arnaumoracoll <arnau.mora@modelical.com>
2024-06-12 18:03:58 +0200

    añadir funcion sum

---
+++ index.js
@@ -0,0 +1,6 @@
+// globalThis: Es una varibale global en toda nuestra aplicación
+
+// Esto sirve para importar la funcion sum
+const sum = require('./sum')
+
+console.log(sum(1,2))
---
+++ sum.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+    return a + b;
+}
+
+// Esto sirve para exportar la funcion sum
+//TODO: comprobar porque falla
+module.exports(sum)
